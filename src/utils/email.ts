9e6ba233604eaf5619b4844c4edf9e4e58b4d6f1
saisandeepkoritala <saisandeep.koritala@gmail.com>
2026-05-30 05:13:21 -0500

    email,pass moved to env

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
